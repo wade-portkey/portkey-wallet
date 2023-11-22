f4e28bd20ba210dcf90f3e8e8c9217d8f243616c
wade-portkey <codingwizard@portkey.finance>
2023-11-22 17:43:01 +0800

    feat: remove mobile-app-did

--- packages/mobile-app-did/js/utils/errorHandler/ExceptionHandler.ts
+++
@@ -1,8 +0,0 @@
-import * as Sentry from '@sentry/react-native';
-import { ExceptionManager } from '@portkey-wallet/utils/ExceptionManager';
-
-class SentryRNExceptionManager extends ExceptionManager {}
-
-const exceptionManager = new SentryRNExceptionManager(Sentry);
-
-export { exceptionManager };
