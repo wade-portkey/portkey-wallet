3807816241ca63f957d6b79bcdc8a89eaf5f7b07
wade-portkey <codingwizard@portkey.finance>
2023-11-27 14:03:11 +0800

    bugfix: fix ios ReceiveTokenPage props type error

--- packages/rn-sdk/js/pages/Assets/ReceiveToken/index.tsx
+++ packages/rn-sdk/js/pages/Assets/ReceiveToken/index.tsx
@@ -23,8 +23,19 @@ export default function ReceiveTokenPage(props: {
   const { t } = useLanguage();
   const { token, currentNetwork, currentCaAddress, defaultToken: defaultTokenJSONStr } = props;
 
-  const tokenObj = JSON.parse(token);
-  const defaultToken = JSON.parse(defaultTokenJSONStr);
+  let tokenObj;
+  console.log('token', typeof token);
+  if (typeof token === 'string') {
+    tokenObj = JSON.parse(token);
+  } else {
+    tokenObj = token;
+  }
+  let defaultToken;
+  if (typeof defaultTokenJSONStr === 'string') {
+    defaultToken = JSON.parse(defaultTokenJSONStr);
+  } else {
+    defaultToken = defaultTokenJSONStr;
+  }
   const { chainId, symbol } = tokenObj;
   const symbolImages = useSymbolImages();
 
