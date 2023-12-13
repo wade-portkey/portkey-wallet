bd59e6f6c6799579b13bb3b007a10672b108aaa0
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2023-12-13 15:46:46 +0800

    fix: logs issue

--- packages/rn-sdk/src/global/init/services.ts
+++ packages/rn-sdk/src/global/init/services.ts
@@ -1,6 +1,7 @@
 import { Platform } from 'react-native';
 import { initHeadlessTasks } from 'service/JsModules/BackgroundTasks';
 import { initJSBatchedBridgeModules } from 'service/JsModules/BatchedBridges';
+import { PortkeyModulesEntity } from 'service/native-modules';
 
 const initJsMethodService = () => {
   if (Platform.OS === 'ios') {
@@ -9,5 +10,9 @@ const initJsMethodService = () => {
     initHeadlessTasks();
   }
 };
+
+ErrorUtils.setGlobalHandler(error => {
+  PortkeyModulesEntity.NativeWrapperModule.onError('js', error.message, error.stack);
+});
 
 export { initJsMethodService };
